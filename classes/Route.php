05ceb7ed96a2b74eb4e4a461633fc76f641582fa
jackwhelan <jackw97@live.ie>
2019-08-23 21:47:44 +0100

    Modified Route structure
    controller/methods[]

--- classes/Route.php
+++ classes/Route.php
@@ -3,17 +3,34 @@
 
     class Route
     {
-        var $components;
+        var $controller;
+        var $methods = array();
 
-        function __construct($url)
+        function __construct($uri)
         {
-            $this->components = explode('/', $url);
-            array_shift($this->components);
+            // Stores the URI arguments in an array called components.
+            $components = explode('/', $uri);
+
+            // Remove the first item of the array, because it is empty by default.
+            array_shift($components);
+
+            // Remove the next item of the array (the first argument of the URI) and store it in the controller field of the Route object.
+            $this->controller = array_shift($components);
+
+            // Add each other URI argument to the methods array.
+            foreach($components as $method)
+            {
+                array_push($this->methods, $method);
+            }
+
+            // Cleanup
+            unset($components);
         }
 
-        function getComponents()
+        // Returns entire route object, i.e. A Controller and an array of Methods.
+        function getRouteInfo()
         {
-            return $this->components;
+            echo "<pre>". var_dump($this) ."</pre>";
         }
     }
 ?>

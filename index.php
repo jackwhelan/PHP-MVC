a2a8643d9dc3689764acc0e28fa963b16743f0da
jackwhelan <jackw97@live.ie>
2019-08-23 19:45:05 +0100

    Added class autoloader

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    spl_autoload_register(function($className) {
+        $ds = DIRECTORY_SEPARATOR;
+        $dir = __DIR__;
+        $className = str_replace('\\', $ds, $className);
+        $file = "{$dir}{$ds}{$className}.php";
+        
+        if(is_readable($file)) {
+            require_once $file;
+        }
+    });
+?>

0d2ccd961bf0767bcde0bd2314d4157d310687c3
jackwhelan <jackw97@live.ie>
2019-08-26 19:07:27 +0100

    Added basic controllers

---
+++ controllers/error_controller.php
@@ -0,0 +1,24 @@
+<?php
+    namespace controllers;
+
+    class error_controller
+    {
+        var $view;
+
+        function __construct($view)
+        {
+            $this->view = $view;
+        }
+
+        function renderView()
+        {
+            if(file_exists($this->view))
+            {
+                include($this->view);
+            }
+            else
+            {
+                return "The view for $this could not be found.";
+            }
+        }
+    }
